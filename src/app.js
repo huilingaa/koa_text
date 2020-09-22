8e74493f810a417e9ba40d3cd58b4a7b1ec40f26
xstxhjh <573363596@qq.com>
2020-09-22 19:27:08 +0800

    中间件

--- src/app.js
+++ src/app.js
@@ -1,8 +1,23 @@
 const Koa = require('koa')
 const os = require('os')
-const Router = require('koa-router')
+const Router = require('@koa-router')
+const cors = require('@koa/cors')
+const CSRF = require('koa-csrf')
+const bodyParser = require('koa-bodyparser')
+
+const multer = require('@koa/multer')
+const upload = multer()
+global.upload = upload
 
 const app = new Koa()
+app.use(cors())
+app.use(new CSRF({
+  invalidTokenMessage: 'Invalid CSRF token',
+  invalidTokenStatusCode: 403,
+  excludedMethods: ['GET', 'HEAD', 'OPTIONS'],
+  disableQuery: false
+}))
+app.use(bodyParser())
 
 /* 路由处理 */
 const router = new Router()
@@ -11,7 +26,6 @@ router.get('/',
     console.log(ctx)
     next()
   })
-
 app
   .use(router.routes())
   .use(router.allowedMethods())
