2af730006b7ae939188cad065829bf8d39aa6607
xstxhjh <573363596@qq.com>
2020-09-22 14:42:08 +0800

    router

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,5 +10,6 @@ module.exports = {
     ecmaVersion: 12
   },
   rules: {
+    'new-cap': 0
   }
 }
--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
 const Koa = require('koa')
 const os = require('os')
+const Router = require('koa-router')
 
 const app = new Koa()
 
-app.use(ctx => {
-  ctx.body = 'Hello Koa'
-})
+/* 路由处理 */
+const router = new Router()
+router.get('/',
+  (ctx, next) => {
+    console.log(ctx)
+    next()
+  })
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods())
 
 /* 启动 */
 const port = 3000
@@ -13,5 +22,5 @@ const network = os.networkInterfaces()
 const localhost = network[Object.keys(network)[0]][1].address
 app.listen(3000, () => {
   console.log(`运行中： http://localhost:${port}`)
-  console.log(`运行中： http://${localhost}:${port}`)
+  console.log(`         http://${localhost}:${port}`)
 })
