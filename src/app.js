52c35641db542a219f977109cccdddbb35efc709
xstxhjh <573363596@qq.com>
2020-09-23 18:36:56 +0800

    jwt

--- src/app.js
+++ src/app.js
@@ -1,31 +1,30 @@
 const Koa = require('koa')
 const os = require('os')
-const Router = require('@koa-router')
+const Router = require('@koa/router')
+const cors = require('@koa/cors')
+const jwt = require('koa-jwt')
 const koaBody = require('koa-body')
 // https://github.com/dlau/koa-body 支持文件上传
-const cors = require('@koa/cors')
-const CSRF = require('koa-csrf')
 
 const app = new Koa()
-app.use(koaBody())
-app.use(cors())
-app.use(new CSRF({
-  invalidTokenMessage: 'Invalid CSRF token',
-  invalidTokenStatusCode: 403,
-  excludedMethods: ['GET', 'HEAD', 'OPTIONS'],
-  disableQuery: false
-}))
-
-/* 路由处理 */
 const router = new Router()
-router.get('/',
-  (ctx, next) => {
-    console.log(ctx)
-    next()
-  })
+app.use(cors())
+app.use(koaBody())
 app
   .use(router.routes())
   .use(router.allowedMethods())
+
+// 不受保护的
+router.get('/', (ctx, next) => {
+  ctx.body = 'hello world'
+})
+
+app.use(jwt({ secret: 'shared-secret' }))
+
+// 受到保护的
+router.get('/token', (ctx, next) => {
+  ctx.body = ctx
+})
 
 /* 启动 */
 const port = 3000
