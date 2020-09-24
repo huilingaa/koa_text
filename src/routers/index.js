368e9b48f5bb146b6dc9ba6fffaa389d377b398b
xstxhjh <573363596@qq.com>
2020-09-24 19:18:31 +0800

    error catch

--- src/routers/index.js
+++ src/routers/index.js
@@ -7,18 +7,25 @@ const dirPath = (url = '') => {
   return path.resolve(__dirname, `../routers/${url}`)
 }
 
-module.exports = async app => {
-  const dirData = fs.readdirSync(dirPath())
-
+const recursion = (path = '') => {
+  const dirData = fs.readdirSync(dirPath(path))
   dirData.map(item => {
-    if (item.includes('.')) return
-    const allApi = fs.readdirSync(dirPath(item))
+    const nextPath = `${path}/${item}`
+    if (item.includes('.')) {
+      const fn = require(dirPath(nextPath))
 
-    allApi.map(aItem => {
-      const fn = require(dirPath(`${item}/${aItem}`))
+      // 防止第一层index死循环
+      if (nextPath == '/index.js') return
+
       if (typeof fn === 'function') fn(router)
-    })
+    } else {
+      recursion(nextPath)
+    }
   })
+}
+
+module.exports = async app => {
+  recursion()
 
   app
     .use(router.routes())
