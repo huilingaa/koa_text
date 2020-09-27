5515e0ab957be4c44049e67a588e265c198d88e6
xstxhjh <573363596@qq.com>
2020-09-27 17:09:44 +0800

    swagger

--- src/routers/user/login.js
+++ src/routers/user/login.js
@@ -3,12 +3,24 @@ const { jsonwebtokenSign } = require('../../plugins/jwt')
 /**
    * @swagger
    * /login:
+   *   required:
+   *     - username
+   *     - password
    *   post:
-   *     description: 获取token
+   *     description: 登录获取token
    *     tags: [User]
+   *     parameters:
+   *       - name: username
+   *         type: string
+   *         required: true
+   *         description: 用户账号
+   *       - name: password
+   *         type: string
+   *         required: true
+   *         description: 用户密码
    *     responses:
    *       200:
-   *         description: 返回用户信息
+   *         description: 返回用户信息及token
    */
 module.exports = async router => {
   router.post('/login', (ctx, next) => {
