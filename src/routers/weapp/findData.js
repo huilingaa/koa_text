232d121473dc0db5211fdf930637175353eacff8
xstxhjh <573363596@qq.com>
2020-10-04 03:59:21 +0800

    查找岗位

--- src/routers/weapp/findData.js
+++ src/routers/weapp/findData.js
@@ -20,4 +20,24 @@ module.exports = async router => {
     ctx.body = jobType
     await next()
   })
+
+  /**
+     * @swagger
+     * /weapp/find_job:
+     *   get:
+     *     description: 小程序获取所有职位
+     *     tags: [weapp]
+     *     responses:
+     *       200:
+     *         description: 职位list
+     *         schema:
+     *           example:
+     *              {"data": [{"name":"职位名","key":"icon"}]}
+  */
+  router.get('/weapp/find_job', async (ctx, next) => {
+    const data = _.cloneDeep(jobType)
+    data[0].name = 'test'
+    ctx.body = 'test'
+    await next()
+  })
 }
