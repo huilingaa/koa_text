6526ce04cb366bc042ba8f96c365d1c536e87f19
xstxhjh <573363596@qq.com>
2020-10-10 19:21:34 +0800

    FastScanner way

--- src/plugins/fastScan.js
+++ src/plugins/fastScan.js
@@ -213,3 +213,13 @@ FastScanner.prototype.search = function search (content, options) {
 }
 
 module.exports = FastScanner
+
+// const FastScanner = require('./plugins/fastScan')
+
+// const words = ['今日头条', '微信', '支付宝']
+// const scanner = new FastScanner(words)
+// const content = '今日头条小程序终于来了，这是继微信、支付宝、百度后，第四个推出小程序功能的App。猫眼电影率先试水，出现在今日头条。'
+// const offWords = scanner.search(content)
+// console.log(offWords)
+// const hits = scanner.hits(content)
+// console.log(hits)
