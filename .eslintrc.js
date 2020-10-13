cd7abdac77e840aa936d4c33bf83bbceca8f2033
xstxhjh <573363596@qq.com>
2020-10-13 12:11:37 +0800

    rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,8 @@ module.exports = {
     ecmaVersion: 12
   },
   rules: {
+    'no-console': process.env.NODE_ENV === 'development' ? 'off' : 'error',
+    'no-debugger': process.env.NODE_ENV === 'development' ? 'off' : 'error',
     'new-cap': 0,
     eqeqeq: 0,
     camelcase: 0,
