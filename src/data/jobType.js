33f771e9ff428d3dfadc3c2bc17fbefc96d7e4a7
xstxhjh <573363596@qq.com>
2020-10-18 01:12:47 +0800

    代码 eslint

--- src/data/jobType.js
+++ src/data/jobType.js
@@ -1,42 +1,42 @@
-let arr = [{
+const arr = [{
   name: '技术类',
   key: 'code'
 }, {
   name: '产品类',
-  key: 'product',
+  key: 'product'
 }, {
   name: '兼职类',
-  key: 'concurrent',
+  key: 'concurrent'
 }, {
   name: '其它类',
-  key: 'other',
+  key: 'other'
 }, {
   name: '设计类',
-  key: 'design',
+  key: 'design'
 }, {
   name: '内容类',
-  key: 'content',
+  key: 'content'
 }, {
   name: '财务类',
-  key: 'finance',
+  key: 'finance'
 }, {
   name: '行政支持类',
-  key: 'administration',
+  key: 'administration'
 }, {
   name: '人力资源类',
-  key: 'humanResources',
+  key: 'humanResources'
 }, {
   name: '营销与公关类',
-  key: 'market',
+  key: 'market'
 }, {
   name: '战略与投资类',
-  key: 'strategy',
+  key: 'strategy'
 }, {
   name: '法律与公共策略类类',
-  key: 'law',
+  key: 'law'
 }, {
   name: '销售、服务与支持类',
-  key: 'sale',
+  key: 'sale'
 }]
 
 module.exports = arr
--- src/model/Apply.js
+++ src/model/Apply.js
@@ -1,30 +1,30 @@
 const mongoose = require('mongoose')
 
 const schema = new mongoose.Schema(
-    {
-        job_name: String, // 职位名称
-        job_type_id: String, // 职位类型key
-        company_name: String, // 公司名称
-        company_address: String, // 公司地址
-        company_size: String, // 公司规模
-        phone: String, // 手机号
-        people: String, // 联系人
-        email: String, // 邮箱
-        content: String, // 富文本 招聘描述
-        status: {
-            type: String,
-            default: '0'
-        }, // 审核状态 0待审核 1通过 2拒绝
-        data_type: String, // 数据类型 1申请 2导入JSON
-        view_total: Number, // 查看统计
-        tags: Array, // 岗位标签
-        files: Array, // 上传文件
-        user_id: { // 账号id
-            type: mongoose.Schema.ObjectId,
-            ref: 'User'
-        }
-    },
-    { timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }, versionKey: false }
+  {
+    job_name: String, // 职位名称
+    job_type_id: String, // 职位类型key
+    company_name: String, // 公司名称
+    company_address: String, // 公司地址
+    company_size: String, // 公司规模
+    phone: String, // 手机号
+    people: String, // 联系人
+    email: String, // 邮箱
+    content: String, // 富文本 招聘描述
+    status: {
+      type: String,
+      default: '0'
+    }, // 审核状态 0待审核 1通过 2拒绝
+    data_type: String, // 数据类型 1申请 2导入JSON
+    view_total: Number, // 查看统计
+    tags: Array, // 岗位标签
+    files: Array, // 上传文件
+    user_id: { // 账号id
+      type: mongoose.Schema.ObjectId,
+      ref: 'User'
+    }
+  },
+  { timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }, versionKey: false }
 )
 
 module.exports = mongoose.model('Apply', schema)
