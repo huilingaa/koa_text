8177590ad9b5c29f569447b2a27590a31a1fbf4d
limingz <739803697@qq.com>
2020-10-18 03:48:13 +0800

    初始数据

--- src/routers/test/index.js
+++ src/routers/test/index.js
@@ -93,7 +93,7 @@ module.exports = async router => {
     const data = await Role.findOneAndUpdate({ name: name }, {
       name: name,
       resume: resume
-    }, { new: true })
+    }, { upsert: true })
     ctx.body = {
       data: data
     }
